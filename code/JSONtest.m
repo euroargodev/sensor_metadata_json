ca03ad208dda82fe145bfa797f253baa1707be2d
Eric Rehm <68403249+SBS-EREHM@users.noreply.github.com>
2023-09-27 13:57:05 -0700

    Remove broken code

--- code/JSONtest.m
+++ code/JSONtest.m
@@ -3,7 +3,7 @@
 % fname = 'sensor-DRUCK-DRUCK_2900PSIA-11698373.json';
 % fname = 'sensor-AANDERAA-AANDERAA_OPTODE_3830-498.json';
 % fname = 'sensor-SBE-SBE41CP-13875.json';
-fname = 'sensor-WETLABS-MCOMS_FLBBCDx-0157.json'; 
+fname = 'sensor-WETLABS-MCOMS_FLBBCD-0157.json'; 
 
 % Read file as binary in one go and then convert to JSON char string 
 fid = fopen(fullfile("examples",fname)); 
@@ -13,20 +13,17 @@ fclose(fid);
 
 % Decode JSON char string into struct
 % val.argo{1:3}, val.vendor{1:2}
-val = jsondecode(str);
+val = jsondecodeEx(str);
 
-N_SENSOR = length(val.argo{1}.SENSOR);
-N_PARAM  = length(val.argo{2}.PARAMETER);
+N_SENSOR = length(val.SENSORS);
+N_PARAM  = length(val.PARAMETERS);
 
-fprintf('SENSOR:\n');
+fprintf('SENSORS:\n');
 for i = 1:N_SENSOR
-    fprintf('%s\n', val.argo{1}.SENSOR{i});
+    fprintf('%s\n', val.SENSORS{i}.SENSOR);
 end
 
-fprintf('\nPARAMETER:\n');
+fprintf('\nPARAMETERS:\n');
 for i = 1:N_PARAM
-    fprintf('%s\n', val.argo{2}.PARAMETER{i});
+    fprintf('%s\n', val.PARAMETERS{i}.PARAMETER);
 end
-
-fprintf(1, '\nvendor:\n');
-fprintf(1, '%s\n', val.vendor{1}.SENSOR_MAKER);
